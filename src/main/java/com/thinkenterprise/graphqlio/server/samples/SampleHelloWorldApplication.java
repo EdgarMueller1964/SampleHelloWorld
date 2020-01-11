216b8b6d710d86c304c3c58c737b662f16cd1ca1
edgar müller <edgar.mueller@msg.group>
2020-01-11 16:37:13 +0100

    first commit

---
+++ src/main/java/com/thinkenterprise/graphqlio/server/samples/SampleHelloWorldApplication.java
@@ -0,0 +1,25 @@
+package com.thinkenterprise.graphqlio.server.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+import com.thinkenterprise.graphqlio.server.EnableGraphQLIOGsLibraryModule;
+import com.thinkenterprise.gts.EnableGraphQLIOGtsLibraryModule;
+import com.thinkenterprise.gtt.EnableGraphQLIOGttLibraryModule;
+import com.thinkenterprise.wsf.EnableGraphQLIOWsfLibraryModule;
+
+@SpringBootApplication
+@Configuration
+@EnableGraphQLIOGsLibraryModule
+@EnableGraphQLIOGtsLibraryModule
+@EnableGraphQLIOGttLibraryModule
+@EnableGraphQLIOWsfLibraryModule
+
+public class SampleHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleHelloWorldApplication.class, args);
+	}
+
+}
